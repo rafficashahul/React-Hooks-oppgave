f590fd5f844657c12f0273029f19962c4b99295f
rafficashahul <raffica26sandefjord@gmail.com>
2026-05-08 11:03:55 +0200

    repo name to image src

--- src/component/CookiesCounter.jsx
+++ src/component/CookiesCounter.jsx
@@ -11,7 +11,7 @@ const Counter = () => {
         className={styles.cookieBtn}
       >
         <img
-          src="public/Cookies.jpg"
+          src="/React-Hooks-oppgave/Cookies.jpg"
           alt="Image of a Cookie"
           className={styles.cookies}
         />
